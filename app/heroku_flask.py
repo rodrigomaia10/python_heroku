787e047174c2254dc0293e9852c13d9f9d1424c5
Rodrigo Maia <sis-rodrigo@hotmail.com>
2020-02-14 14:33:01 -0300

    Fist commit

---
+++ app/heroku_flask.py
@@ -0,0 +1,15 @@
+importa os
+from flask import flask
+
+app flask(_name_)
+
+@app_route('/')
+
+def nao_entre_em_panico():
+    if request.headers.get('Authorization') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "Não entre em pânico!"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
